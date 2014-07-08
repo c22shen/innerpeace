f3ef5878b858abf5d878f11f4a689c48d02cf5f2
Xiao Shen <xiaoshen2009@gmail.com>
2014-07-07 23:50:09 -0700

    sort by date

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,7 +1,7 @@
 class ContestsController < ApplicationController
 def show
-  @xiao_status = Status.where(:user => "xiao")
-  @henry_status = Status.where(:user => "henry")
+  @xiao_status = Status.where(:user => "xiao").sort_by(&:created_at)
+  @henry_status = Status.where(:user => "henry").sort_by(&:created_at)
   @status = Status.new
 end
 end
