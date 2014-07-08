059ff41b1ec4bfc0f91907f3f481aa243511a1de
Xiao Shen <xiaoshen2009@gmail.com>
2014-07-08 00:43:19 -0700

    fix

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,7 +1,7 @@
 class StatusesController < ApplicationController
   def create
     status = Status.new(params[:status])
-    unless params[:status].message.blank?
+    unless params[:status][:message].blank?
       status.save
     end
     redirect_to root_path
